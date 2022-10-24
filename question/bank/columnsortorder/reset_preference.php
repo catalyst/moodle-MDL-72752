lucaboesch/moodle

MDL-74054 qbank_columnsortorder: User Configurable view
This commit will implement user configurable question
bank view for the users in the question management page.
This commit also ensures specific api usage can have
their own configurable view for specific users.

question/bank/columnsortorder/reset_preference.php
@@ -1,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Question bank settings page class.
+ *
+ * @package    qbank_columnsortorder
+ * @copyright  2022 Catalyst IT Australia Pty Ltd
+ * @author     Nathan Nguyen <nathannguyen@catalyst-ca.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . '/../../../config.php');
+
+$returnurl = required_param('returnurl', PARAM_LOCALURL);
+$preference = required_param('preference', PARAM_TEXT);
+
+require_sesskey();
+require_login();
+
+// Reset user preference.
+unset_user_preference("${preference}_enabledcol");
+unset_user_preference("${preference}_pinnedcols");
+unset_user_preference("${preference}_hiddencols");
+unset_user_preference("${preference}_colsize");
+
+redirect($returnurl);

question/classes/local/bank/view.php
@@ -375,8 +375,16 @@ class view {
         }
 
         // Check if qbank_columnsortorder is enabled.
+        // TODO: Extract this into a function, so that it is easier to override.
         if (array_key_exists('columnsortorder', core_plugin_manager::instance()->get_enabled_plugins('qbank'))) {
-            $columnorder = new column_manager();
+            if (!$this->course->id) {
+                // Site admin context.
+                $preference = "";
+            } else {
+                // Course or module context.
+                $preference = $this->component;
+            }
+            $columnorder = new column_manager($preference);
             $questionbankclasscolumns = $columnorder->get_sorted_columns($questionbankclasscolumns);
         }
 
@@ -399,7 +407,7 @@ class view {
         $this->requiredcolumns = [];
         $questionbankcolumns = $this->get_question_bank_plugins();
         foreach ($questionbankcolumns as $classobject) {
-            if (empty($classobject)) {
+            if (empty($classobject) || !($classobject instanceof \core_question\local\bank\column_base)) {
                 continue;
             }
             $this->requiredcolumns[$classobject->get_column_name()] = $classobject;
@@ -436,6 +444,7 @@ class view {
      * @param string $heading The name of column that is set as heading
      */
     protected function init_columns($wanted, $heading = ''): void {
+        global $PAGE;
         // If we are using the edit menu column, allow it to absorb all the actions.
         foreach ($wanted as $column) {
             if ($column instanceof edit_menu_column) {
@@ -451,7 +460,10 @@ class view {
             if ($column->is_extra_row()) {
                 $this->extrarows[$column->get_column_name()] = $column;
             } else {
-                $this->visiblecolumns[$column->get_column_name()] = $column;
+                // Only add columns which are visible.
+                if ($PAGE->user_is_editing() || $column->isvisible) {
+                    $this->visiblecolumns[$column->get_column_name()] = $column;
+                }
             }
         }
 
@@ -1186,8 +1198,36 @@ class view {
      * @param array $questions
      */
     protected function print_table($questions): void {
+        global $PAGE;
+
+        // Column actions.
+        $columnsortorder = new column_manager($this->component);
+        $pinnedcolumns = $columnsortorder->pinnedcolumns;
+        $hiddencolumns = $columnsortorder->hiddencolumns;
+        $colsize = $columnsortorder->colsize;
+
+        if ($PAGE->user_is_editing()) {
+            // Reset link.
+            $url = new \moodle_url('/question/bank/columnsortorder/reset_preference.php', [
+                'returnurl' => $this->returnurl,
+                'preference' => $this->component,
+                'sesskey' => sesskey()
+            ]);
+            $resetlink = \html_writer::link($url, get_string('reset'), ['class' => "btn btn-secondary mr-1"]);
+            $resetlink = \html_writer::div($resetlink, '');
+            // Container for show/hide dropdown.
+            $dropdown = \html_writer::div('', '', ['id' => "show-hide-dropdown"]);
+            echo \html_writer::div( $resetlink . $dropdown, '', ['class' => "row justify-content-end"]);
+        }
+
         // Start of the table.
-        echo \html_writer::start_tag('table', ['id' => 'categoryquestions', 'class' => 'table-responsive']);
+        echo \html_writer::start_tag('table', [
+            'id' => 'categoryquestions',
+            'class' => 'table-responsive',
+            'data-pinnedcolumns' => json_encode($pinnedcolumns),
+            'data-hiddencolumns' => json_encode($hiddencolumns),
+            'data-colsize' => $colsize,
+        ]);
 
         // Prints the table header.
         echo \html_writer::start_tag('thead');
@@ -1207,6 +1247,10 @@ class view {
 
         // End of the table.
         echo \html_writer::end_tag('table');
+
+        // Column Action script.
+        $PAGE->requires->js_call_amd('qbank_columnsortorder/qbank_table_action', 'init',
+            [$PAGE->user_is_editing(), $this->component]);
     }
 
     /**

catalyst/moodle-MDL-72752

Co-Authored-By: Nathan Nguyen <nathannguyen@catalyst-au.net>
Co-Authored-By: Safat Shahin <safatshahin@catalyst-au.net>

question/bank/columnsortorder/reset_preference.php
@@ -1,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Question bank settings page class.
+ *
+ * @package    qbank_columnsortorder
+ * @copyright  2022 Catalyst IT Australia Pty Ltd
+ * @author     Nathan Nguyen <nathannguyen@catalyst-ca.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(__DIR__ . '/../../../config.php');
+
+$returnurl = required_param('returnurl', PARAM_LOCALURL);
+$component = required_param('component', PARAM_TEXT);
+
+require_sesskey();
+require_login();
+
+// Reset user preference.
+unset_user_preference("${component}_enabledcol");
+unset_user_preference("${component}_pinnedcols");
+unset_user_preference("${component}_hiddencols");
+unset_user_preference("${component}_colsize");
+
+redirect($returnurl);

question/classes/local/bank/view.php
@@ -375,8 +375,16 @@ class view {
         }
 
         // Check if qbank_columnsortorder is enabled.
+        // TODO: Extract this into a function, so that it is easier to override.
         if (array_key_exists('columnsortorder', core_plugin_manager::instance()->get_enabled_plugins('qbank'))) {
-            $columnorder = new column_manager();
+            if (!$this->course->id) {
+                // Site admin context.
+                $preference = "";
+            } else {
+                // Course or module context.
+                $preference = $this->component;
+            }
+            $columnorder = new column_manager($preference);
             $questionbankclasscolumns = $columnorder->get_sorted_columns($questionbankclasscolumns);
         }
 
@@ -399,7 +407,7 @@ class view {
         $this->requiredcolumns = [];
         $questionbankcolumns = $this->get_question_bank_plugins();
         foreach ($questionbankcolumns as $classobject) {
-            if (empty($classobject)) {
+            if (empty($classobject) || !($classobject instanceof \core_question\local\bank\column_base)) {
                 continue;
             }
             $this->requiredcolumns[$classobject->get_column_name()] = $classobject;
@@ -436,6 +444,7 @@ class view {
      * @param string $heading The name of column that is set as heading
      */
     protected function init_columns($wanted, $heading = ''): void {
+        global $PAGE;
         // If we are using the edit menu column, allow it to absorb all the actions.
         foreach ($wanted as $column) {
             if ($column instanceof edit_menu_column) {
@@ -451,7 +460,10 @@ class view {
             if ($column->is_extra_row()) {
                 $this->extrarows[$column->get_column_name()] = $column;
             } else {
-                $this->visiblecolumns[$column->get_column_name()] = $column;
+                // Only add columns which are visible.
+                if ($PAGE->user_is_editing() || $column->isvisible) {
+                    $this->visiblecolumns[$column->get_column_name()] = $column;
+                }
             }
         }
 
@@ -1186,8 +1198,36 @@ class view {
      * @param array $questions
      */
     protected function print_table($questions): void {
+        global $PAGE;
+
+        // Column actions.
+        $columnsortorder = new column_manager($this->component);
+        $pinnedcolumns = $columnsortorder->pinnedcolumns;
+        $hiddencolumns = $columnsortorder->hiddencolumns;
+        $colsize = $columnsortorder->colsize;
+
+        if ($PAGE->user_is_editing()) {
+            // Reset link.
+            $url = new \moodle_url('/question/bank/columnsortorder/reset_preference.php', [
+                'returnurl' => $this->returnurl,
+                'component' => $this->component,
+                'sesskey' => sesskey()
+            ]);
+            $resetlink = \html_writer::link($url, get_string('reset'), ['class' => "btn btn-secondary mr-1"]);
+            $resetlink = \html_writer::div($resetlink, '');
+            // Container for show/hide dropdown.
+            $dropdown = \html_writer::div('', '', ['id' => "show-hide-dropdown"]);
+            echo \html_writer::div( $resetlink . $dropdown, '', ['class' => "row justify-content-end"]);
+        }
+
         // Start of the table.
-        echo \html_writer::start_tag('table', ['id' => 'categoryquestions', 'class' => 'table-responsive']);
+        echo \html_writer::start_tag('table', [
+            'id' => 'categoryquestions',
+            'class' => 'table-responsive',
+            'data-pinnedcolumns' => json_encode($pinnedcolumns),
+            'data-hiddencolumns' => json_encode($hiddencolumns),
+            'data-colsize' => $colsize,
+        ]);
 
         // Prints the table header.
         echo \html_writer::start_tag('thead');
@@ -1207,6 +1247,10 @@ class view {
 
         // End of the table.
         echo \html_writer::end_tag('table');
+
+        // Column Action script.
+        $PAGE->requires->js_call_amd('qbank_columnsortorder/qbank_table_action', 'init',
+            [$PAGE->user_is_editing(), $this->component]);
     }
 
     /**